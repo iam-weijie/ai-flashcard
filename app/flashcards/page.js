57c31e96aa2d9110ed4dd332c5e9404466870a90
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-14 12:51:39 -0400

    flashcards overview

---
+++ app/flashcards/page.js
@@ -0,0 +1,44 @@
+export default function Flashcards() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+  const router = useRouter();
+
+  const handleCardClick = (id) => {
+    router.push(`/flashcard?id=${id}`);
+  };
+
+  useEffect(() => {
+    async function getFlashcards() {
+      if (!user) return;
+      const docRef = doc(collection(db, "users"), user.id);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const collections = docSnap.data().flashcards || [];
+        setFlashcards(collections);
+      } else {
+        await setDoc(docRef, { flashcards: [] });
+      }
+    }
+    getFlashcards();
+  }, [user]);
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3} sx={{ mt: 4 }}>
+        {flashcards.map((flashcard, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {flashcard.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
