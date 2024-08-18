2f0717bbbdcfb50494552999b2ebf64bd3468886
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-17 20:10:31 -0400

    adapt Card component for nextjs

--- components/Card.js
+++ components/Card.js
@@ -1,32 +1,28 @@
 import { animated, to } from "@react-spring/web";
-import React from "react";
 
-class Card extends React.Component {
-  render() {
-    const { i, x, y, rot, scale, trans, bind, data } = this.props;
-    const { front, back } = data[i];
+const Card = ({ i, x, y, rot, scale, trans, bind, data }) => {
+  const { front, back } = data[i];
 
-    return (
+  return (
+    <animated.div
+      key={i}
+      style={{
+        transform: to([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`),
+      }}
+    >
       <animated.div
-        key={i}
+        {...bind(i)}
         style={{
-          transform: to([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`),
+          transform: to([rot, scale], trans),
         }}
       >
-        <animated.div
-          {...bind(i)}
-          style={{
-            transform: to([rot, scale], trans),
-          }}
-        >
-          <div className="card">
-            <h2> {front} </h2>
-            <h2> {back} </h2>
-          </div>
-        </animated.div>
+        <div className="card">
+          <h2> {front} </h2>
+          <h2> {back} </h2>
+        </div>
       </animated.div>
-    );
-  }
-}
+    </animated.div>
+  );
+};
 
 export default Card;
